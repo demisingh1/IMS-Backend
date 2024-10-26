c8c94ba7bb04db2d53bb10b95c83382ba994a74c
demisingh1 <jaswinder.cyrus1@gmail.com>
2024-10-26 15:55:56 -0400

    socket Implimentations CORS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 require('dotenv').config();
 const express = require('express')
-const app = express()
+const http = require('http')
+const {Server} = require('socket.io')
 
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server,{
+    cors:{
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'PATCH', 'POST', 'PUT','DELETE'],
+    }
+});
+
+// initial socket connection
+io.on("connection", (socket)=>{
+    console.log("Socket connected with id  ", socket.id);
+    
+})
 
 
 
 
 // server listen
 const port = process.env.PORT || 8001
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`server Started on port ${port}`);
     
 })
